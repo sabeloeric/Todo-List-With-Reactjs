f11df0d8aa55496a6c48c969624dfd1a7727a617
Sabelo Thabethe <thbsab001@myuct.ac.za>
2020-07-16 01:26:53 +0200

    styled AddTodo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Form, Col, Button, Container } from "react-bootstrap";
 
 export default function AddTodo() {
-    return (
-        <div>
-            add todo
-        </div>
-    )
+  return (
+    <Container>
+      <Form>
+        <Form.Row>
+          <Col>
+            <Form.Control placeholder="Enter your task"></Form.Control>
+          </Col>
+          <Col>
+            <Button>Submit</Button>
+          </Col>
+        </Form.Row>
+      </Form>
+    </Container>
+  );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { ListGroup } from 'react-bootstrap'
+import React from "react";
+import { ListGroup, Button, Container, Col, Row } from "react-bootstrap";
 
-export default function Todo ({todos}){
-    return (
-        <div>
-          {todos.map((todo)=>{
-              return <ListGroup key={todo.id} > 
-                <ListGroup.Item variant="primary">{todo.title}</ListGroup.Item> 
-              </ListGroup>
-          })}
-        </div>
-    )
+export default function Todo({ todos }) {
+  return (
+    <div>
+      {todos.map((todo) => {
+        return (
+          <Container key={todo.id}>
+            <Row>
+              <Col>{todo.title}</Col>
+              <Col xs={3}>
+                <Button variant="outline-danger">delete</Button>
+              </Col>
+            </Row>
+          </Container>
+        );
+      })}
+    </div>
+  );
 }
-
